0632852273d482c82911ef973ba998c5e9407250
Giuseppe Raffa <giuse.raffa.0@gmail.com>
2025-10-12 12:59:31 +0200

    exdended limit for element uploads, added caches

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(
   }),
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 //For middleware
@@ -81,6 +81,7 @@ app.use(errorHandler);
 app.use((req, res) => {
   res.status(404).json({
     message: "Invalid route",
+    route: req.originalUrl,
   });
 });
 
